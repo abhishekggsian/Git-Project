6cf00e602b6c7ebf7a20d8508e384fb170d5f573
devopsannu <devopsannu@gmail.com>
2025-05-29 13:40:34 +0530

    Written the code for storage account

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ resource "azurerm_resource_group" "rg-env" {
      location="eastus"
   
 }
+
+
+resource "azurerm_storage_account" "sa-env" {
+  name                     = "stenv001"
+  resource_group_name      = "rg-env-001"
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
